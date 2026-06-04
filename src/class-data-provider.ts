fa5897bdf1229e1ebc8daa31f837c9856a5a49da
danielz0102 <danyzglez@hotmail.com>
2026-06-04 17:19:28 -0600

    feat: render root classes

--- src/class-data-provider.ts
+++ src/class-data-provider.ts
@@ -1,16 +1,37 @@
 import * as vscode from "vscode"
+import { parseCSSClasses } from "./parse-css-classes"
 
 export class ClassDataProvider implements vscode.TreeDataProvider<ClassItem> {
   getTreeItem(element: ClassItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
     return element
   }
 
-  getChildren(element?: ClassItem): vscode.ProviderResult<ClassItem[]> {
+  async getChildren(element?: ClassItem): Promise<ClassItem[]> {
     if (element) {
       return [new ClassItem("Class 1"), new ClassItem("Class 2"), new ClassItem("Class 3")]
     }
 
-    return [new ClassItem("Class 1"), new ClassItem("Class 2"), new ClassItem("Class 3")]
+    const cssFiles = await this.getCSSFiles()
+    const classes = await this.getCSSClasses(cssFiles)
+
+    return classes.map((c) => new ClassItem(`.${c}`))
+  }
+
+  private async getCSSFiles(): Promise<vscode.Uri[]> {
+    return await vscode.workspace.findFiles("**/*.css", "**/node_modules/**")
+  }
+
+  private async getCSSClasses(files: vscode.Uri[]): Promise<string[]> {
+    const classesPromises = files.map(async (file) => {
+      const content = await this.getCSSFileContent(file)
+      return await parseCSSClasses(content)
+    })
+    return await Promise.all(classesPromises).then((r) => r.flat())
+  }
+
+  private async getCSSFileContent(file: vscode.Uri): Promise<string> {
+    const content = await vscode.workspace.fs.readFile(file)
+    return content.toString()
   }
 }
 
