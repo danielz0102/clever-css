d1c4adb7725cd35ef47ca392aacca15f8004802f
danielz0102 <danyzglez@hotmail.com>
2026-06-05 15:34:16 -0600

    refactor: remove Hello World command

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,6 @@ import * as vscode from "vscode"
 import { ClassDataProvider } from "./class-data-provider"
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand("css-viewer.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World from css-viewer!")
-  })
-
-  context.subscriptions.push(disposable)
-
   const openClassCommand = vscode.commands.registerCommand(
     "css-viewer.openClass",
     async (uri: vscode.Uri, range: vscode.Range) => {
