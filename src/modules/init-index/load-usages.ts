811203df67cdec707d1fdb48ae6b2aa9a6b027ca
danielz0102 <danyzglez@hotmail.com>
2026-06-22 16:32:19 -0600

    refactor: extract client file parser

--- src/modules/init-index/load-usages.ts
+++ src/modules/init-index/load-usages.ts
@@ -1,16 +1,10 @@
-import { Project, SyntaxKind, type TemplateExpression } from "ts-morph"
 import * as vscode from "vscode"
 
 import type { CSSClassRepository } from "../../domain/css-class-repository"
-
-type Usage = {
-  name: string
-  start: number
-  end: number
-}
+import { ClientFileParser } from "../../shared/client-file-parser"
 
 export class LoadUsages {
-  private project = new Project()
+  private parser = new ClientFileParser()
 
   constructor(private classes: CSSClassRepository) {}
 
@@ -20,7 +14,7 @@ export class LoadUsages {
     await Promise.all(
       files.map(async (uri) => {
         const document = await vscode.workspace.openTextDocument(uri)
-        const usages = this.parseFile(document.fileName)
+        const usages = this.parser.getUsagesFrom(document.fileName)
 
         usages.forEach(({ name, start, end }) => {
           const cssClass = this.classes.get(name)
@@ -36,70 +30,5 @@ export class LoadUsages {
         })
       })
     )
-  }
-
-  private parseFile(path: string): Usage[] {
-    const ast = this.project.addSourceFileAtPath(path)
-    const jsxAttributes = ast.getDescendantsOfKind(SyntaxKind.JsxAttribute)
-    const usages: Usage[] = []
-
-    jsxAttributes.forEach((attr) => {
-      if (attr.getNameNode().getText() !== "className") return
-
-      const initializer = attr.getInitializer()
-
-      if (!initializer) return
-
-      if (initializer.isKind(SyntaxKind.StringLiteral)) {
-        usages.push(...this.parseText(initializer.getLiteralValue(), initializer.getStart()))
-      } else if (initializer.isKind(SyntaxKind.JsxExpression)) {
-        const expression = initializer.getExpression()
-
-        if (!expression) return
-
-        if (expression.isKind(SyntaxKind.NoSubstitutionTemplateLiteral)) {
-          usages.push(...this.parseText(expression.getLiteralValue(), expression.getStart()))
-        } else if (expression.isKind(SyntaxKind.TemplateExpression)) {
-          usages.push(...this.parseTemplateExpression(expression))
-        }
-      }
-    })
-
-    return usages
-  }
-
-  private parseText(text: string, startOffset: number): Usage[] {
-    const names = text.split(/\s+/)
-    let offset = 0
-
-    const usages: Usage[] = []
-
-    names.forEach((name) => {
-      const idx = text.indexOf(name, offset)
-      const start = startOffset + idx + 1
-      const end = start + name.length
-
-      usages.push({ name, start, end })
-      offset = idx + name.length
-    })
-
-    return usages
-  }
-
-  private parseTemplateExpression(expression: TemplateExpression): Usage[] {
-    const usages: Usage[] = []
-
-    const head = expression.getHead()
-    usages.push(...this.parseText(head.getLiteralText(), head.getStart()))
-
-    expression.getTemplateSpans().forEach((span) => {
-      const literal = span.getLiteral()
-
-      if (literal.isKind(SyntaxKind.TemplateMiddle) || literal.isKind(SyntaxKind.TemplateTail)) {
-        usages.push(...this.parseText(literal.getLiteralText(), literal.getStart()))
-      }
-    })
-
-    return usages
   }
 }
---
+++ src/shared/client-file-parser.ts
@@ -0,0 +1,76 @@
+import { Project, SyntaxKind, type TemplateExpression } from "ts-morph"
+
+type Usage = {
+  name: string
+  start: number
+  end: number
+}
+
+export class ClientFileParser {
+  private project = new Project()
+
+  getUsagesFrom(path: string): Usage[] {
+    const ast = this.project.addSourceFileAtPath(path)
+    const jsxAttributes = ast.getDescendantsOfKind(SyntaxKind.JsxAttribute)
+    const usages: Usage[] = []
+
+    jsxAttributes.forEach((attr) => {
+      if (attr.getNameNode().getText() !== "className") return
+
+      const initializer = attr.getInitializer()
+
+      if (!initializer) return
+
+      if (initializer.isKind(SyntaxKind.StringLiteral)) {
+        usages.push(...this.parseText(initializer.getLiteralValue(), initializer.getStart()))
+      } else if (initializer.isKind(SyntaxKind.JsxExpression)) {
+        const expression = initializer.getExpression()
+
+        if (!expression) return
+
+        if (expression.isKind(SyntaxKind.NoSubstitutionTemplateLiteral)) {
+          usages.push(...this.parseText(expression.getLiteralValue(), expression.getStart()))
+        } else if (expression.isKind(SyntaxKind.TemplateExpression)) {
+          usages.push(...this.parseTemplateExpression(expression))
+        }
+      }
+    })
+
+    return usages
+  }
+
+  private parseText(text: string, startOffset: number): Usage[] {
+    const names = text.split(/\s+/)
+    let offset = 0
+
+    const usages: Usage[] = []
+
+    names.forEach((name) => {
+      const idx = text.indexOf(name, offset)
+      const start = startOffset + idx + 1
+      const end = start + name.length
+
+      usages.push({ name, start, end })
+      offset = idx + name.length
+    })
+
+    return usages
+  }
+
+  private parseTemplateExpression(expression: TemplateExpression): Usage[] {
+    const usages: Usage[] = []
+
+    const head = expression.getHead()
+    usages.push(...this.parseText(head.getLiteralText(), head.getStart()))
+
+    expression.getTemplateSpans().forEach((span) => {
+      const literal = span.getLiteral()
+
+      if (literal.isKind(SyntaxKind.TemplateMiddle) || literal.isKind(SyntaxKind.TemplateTail)) {
+        usages.push(...this.parseText(literal.getLiteralText(), literal.getStart()))
+      }
+    })
+
+    return usages
+  }
+}
