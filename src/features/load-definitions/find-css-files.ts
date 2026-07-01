493d302a86cd9d6251c06b762ddbe4057bac4fbd
danielz0102 <danyzglez@hotmail.com>
2026-07-01 01:31:25 -0600

    refactor: inline folder

--- src/features/load-definitions/find-css-files.ts
+++ src/features/load-definitions/find-css-files.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode"
 
-import { uriToCssFileDto, type CssFileDto } from "../../../dtos/css-file-dto"
+import { uriToCssFileDto, type CssFileDto } from "../../dtos/css-file-dto"
 
 export async function findCssFiles(): Promise<CssFileDto[]> {
   const uris = await vscode.workspace.findFiles("**/*.css", "**/{node_modules,dist,build}/**")
