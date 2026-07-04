e023d4412fc1dfd9656bfb9d038784f453f8838c
danielz0102 <danyzglez@hotmail.com>
2026-07-03 23:52:57 -0600

    fix: convert positions of CSS Tree nodes to 0-based indexing

--- src/adapters/css-parser.ts
+++ src/adapters/css-parser.ts
@@ -1,5 +1,6 @@
 import * as csstree from "css-tree"
 
+import type { Position } from "../domain/location"
 import type { CssFileDto } from "../dtos/css-file-dto"
 import type { Token } from "../dtos/token-dto"
 
@@ -16,10 +17,26 @@ export const parseCssClassTokens: CssClassParser = async (file: CssFileDto) => {
 
       symbols.push({
         name: node.name,
-        location: { uri: file.uri, start: node.loc.start, end: node.loc.end },
+        location: {
+          uri: file.uri,
+          start: toZeroBased(node.loc.start),
+          end: toZeroBased(node.loc.end),
+        },
       })
     },
   })
 
   return symbols
+}
+
+/**
+ * Converts a 1-based position to a 0-based position.
+ * @param position The 1-based position to convert.
+ * @returns The converted 0-based position.
+ */
+function toZeroBased(position: Position): Position {
+  return {
+    line: position.line - 1,
+    column: position.column - 1,
+  }
 }
