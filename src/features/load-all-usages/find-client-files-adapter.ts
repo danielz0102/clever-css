3fe10727d5cf992413cfe0c79cf3e95a002a86c2
danielz0102 <danyzglez@hotmail.com>
2026-07-09 16:31:33 -0600

    feat: select parser based on file extension

--- src/features/load-all-usages/find-client-files-adapter.ts
+++ src/features/load-all-usages/find-client-files-adapter.ts
@@ -1,17 +1,38 @@
 import * as vscode from "vscode"
 
+import type { ClientFileParser } from "../../adapters/client-file-parsers/client-file-parser-port"
+import { HtmlParser } from "../../adapters/client-file-parsers/html-parser-adapter"
 import { JsxParser } from "../../adapters/client-file-parsers/jsx-parser-adapter"
 import type { Token } from "../../dtos/token-dto"
 
-export type ClientFilesFinder = () => Promise<string[]>
-
 export async function parseAllUsages(): Promise<Token[]> {
   const files = await findClientFiles()
-  const parser = new JsxParser()
-  return files.flatMap((uri) => parser.parseUsagesFrom(uri))
+
+  return files.flatMap((uri) => {
+    const extension = uri.split(".").pop()
+    if (!extension) {
+      throw new Error(`File ${uri} has no extension`)
+    }
+
+    return selectParser(extension).parseUsagesFrom(uri)
+  })
 }
 
-export const findClientFiles: ClientFilesFinder = async () => {
-  const files = await vscode.workspace.findFiles("**/*.{jsx,tsx}", "**/node_modules/**")
+async function findClientFiles(): Promise<string[]> {
+  const files = await vscode.workspace.findFiles("**/*.{jsx,tsx,html}", "**/node_modules/**")
   return files.map((uri) => uri.fsPath)
+}
+
+const parsers: Record<string, ClientFileParser> = {
+  jsx: new JsxParser(),
+  tsx: new JsxParser(),
+  html: new HtmlParser(),
+}
+
+function selectParser(extension: string): ClientFileParser {
+  const parser = parsers[extension]
+  if (!parser) {
+    throw new Error(`No parser found for extension ${extension}`)
+  }
+  return parser
 }
