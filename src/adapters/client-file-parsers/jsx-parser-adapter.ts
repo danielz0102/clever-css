6ae4b33d9433eeb46c5e2969609e901f66d65ee2
danielz0102 <danyzglez@hotmail.com>
2026-07-09 01:42:00 -0600

    refactor: remove `removeCache` method from `JsxParser` class
    We can just create a new `Project` instance every time we parse a file

--- src/adapters/client-file-parsers/jsx-parser-adapter.ts
+++ src/adapters/client-file-parsers/jsx-parser-adapter.ts
@@ -11,19 +11,9 @@ import type { Token } from "../../dtos/token-dto"
 import type { ClientFileParser } from "./client-file-parser-port"
 
 export class JsxParser implements ClientFileParser {
-  private readonly project = new Project()
-
   parseUsagesFrom(uri: string): Token[] {
-    this.removeCache(uri)
-    const sourceFile = this.project.addSourceFileAtPath(uri)
+    const sourceFile = new Project().addSourceFileAtPath(uri)
     return new JsxFileParser(sourceFile).parse()
-  }
-
-  private removeCache(uri: string): void {
-    const existing = this.project.getSourceFile(uri)
-    if (existing) {
-      this.project.removeSourceFile(existing)
-    }
   }
 }
 
