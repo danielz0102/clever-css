7184c2dee661a6456ef0eb568f094f251876f250
danielz0102 <danyzglez@hotmail.com>
2026-07-11 21:35:13 -0600

    refactor: replace `css-tree` with `@ast-grep/napi` for CSS parsing

--- src/adapters/css-parser.ts
+++ src/adapters/css-parser.ts
@@ -1,30 +1,21 @@
-import * as csstree from "css-tree"
+import { Lang, parse } from "@ast-grep/napi"
 
 import type { CssFileDto } from "../dtos/css-file-dto"
 import type { Token } from "../dtos/token-dto"
-import { toZeroBased } from "../shared/to-zero-based"
-
 export type CssClassParser = (file: CssFileDto) => Promise<Token[]>
 
 export const parseCssClassTokens: CssClassParser = async (file: CssFileDto) => {
-  const ast = csstree.parse(file.content, { positions: true })
-  const symbols: Token[] = []
-
-  csstree.walk(ast, {
-    visit: "ClassSelector",
-    enter(node) {
-      if (!node.loc) return
-
-      symbols.push({
-        name: node.name,
-        location: {
-          uri: file.uri,
-          start: toZeroBased(node.loc.start),
-          end: toZeroBased(node.loc.end),
-        },
-      })
-    },
+  const ast = parse(Lang.Css, file.content)
+  const nodes = ast.root().findAll({ rule: { kind: "class_name" } })
+  return nodes.flatMap((node) => {
+    const range = node.range()
+    return {
+      name: node.text(),
+      location: {
+        uri: file.uri,
+        start: range.start,
+        end: range.end,
+      },
+    }
   })
-
-  return symbols
 }
